57b5f087a5e8b7a57e72cd9139d6b6ca3dc7f7e6
boussedraoui <boussedraoui.mohamed@gmail.com>
2026-09-26 02:19:21 +0000

    Update service worker cache version to v6

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page-v5";
+const CACHE = "pwabuilder-page-v6";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
